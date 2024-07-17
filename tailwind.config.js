ab0b9178462f48ddfb6c2440e253b29fc554f1df
Georginio-prod <etonameklou6@gmail.com>
2024-07-17 16:45:57 +0000

    style : ajust the style for panel

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
       colors: {
         customGray: '#4C545C',
         gray: '#9EABB2',
-      }
+        ios:'#26BBA4',
+        mac:'#6174FF'
+      },
+      letterSpacing :{
+        '-0.59': '-0.59px',
+      },
+      spacing: {
+        '150': '150px', 
+      },
+      spacing: {
+        '730': '730px', 
+      },
     },
   },
   plugins: [],
